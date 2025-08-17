bfb3bccd297b919e7e315e8a6034feeca9c45987
Everest Paudel <145990676+everestp@users.noreply.github.com>
2025-08-17 21:43:15 +0545

    Create 23-hashmap.rs

---
+++ src/23-hashmap.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+fn main(){
+    let mut users = HashMap::new();
+    users.insert(String::from("Everest"),22);
+    users.insert(String::from("raman"),32);
+    /*
+    what this hash map look like
+       Everest :22,
+       raman :32,
+       
+    
+    */
+    let first_user_age = users.get("Everest");
+    println!("user age ={:?}",first_user_age);
+}
