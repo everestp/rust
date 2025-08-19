1a3d36524e5cadeb14ea85f600c5ad5f69c560f8
Everest Paudel <145990676+everestp@users.noreply.github.com>
2025-08-19 12:23:22 +0545

    Create 35_practice_iterator_ownership.rs

---
+++ src/35_practice_iterator_ownership.rs
@@ -0,0 +1,54 @@
+
+//Write a program to first filter all odd value then double each value and crate a new vector 
+
+
+fn main() {
+   let   v1 = vec![1,2,3,4,5];
+   let  mut new_vec = Vec::new();
+   for  i in v1.iter().filter(|x| *x %2 ==0 ).map(|x| x*2 ) {
+   
+     new_vec.push(i);
+   }
+   let   vec = vec![1,2,3,4,5,6];
+   let ans = filter_and_map(vec.clone()); // use clone because we use it line 16  to again call the function while calling by actaul it transfer the ownership
+   println!("This is the answer->{:?}",ans);
+   println!(" This is the new_vec{:?}",ans);
+   println!(" Can we  directly call and print let see{:?}",filter_and_map(vec));
+     println!(" Yes we can {:?}",ans);
+   filter_and_map(v1.clone());
+   even_file_with_Some(v1.clone());
+   even_filer(v1.clone());
+}
+
+
+
+fn filter_and_map( vec:Vec<i32>)->Vec<i32>{
+    let  new_iter = vec.iter().filter(|x| *x %2==0).map(|x| x*2);
+    let new_vec :Vec<i32> = new_iter.collect();
+    return  new_vec;
+} 
+
+
+fn  even_filer(v:Vec<i32>){
+    let mut filter_even_vec :Vec<i32> = Vec::new();
+     for val in v.iter() {
+        if val  % 2==0{
+
+        filter_even_vec.push(*val);
+        }
+     }
+      println!("this is doeb byu even_filter{:?}",filter_even_vec);
+}
+
+
+fn even_file_with_Some(v:Vec<i32>){
+     let mut filter_even_vec :Vec<i32> = Vec::new();
+     if let Some(val) = v.iter().next() {
+        if val  % 2==0{
+
+        filter_even_vec.push(*val);
+        }
+     }
+      println!("this is doeb byu even_filter_Some{:?}",filter_even_vec);
+
+}
