1c209c61e85674a63c38cbfc1db5f66f3c5a7cd8
Everest Paudel <145990676+everestp@users.noreply.github.com>
2025-08-20 20:42:54 +0545

    Create 38_slice.rs

---
+++ src/38_slice.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+
+    let name = String::from("Everest Paiudel");
+    let  fistname= &name[0..9];
+    println!("Hey this is the first name {}",fistname);
+}
